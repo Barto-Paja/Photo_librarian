b835d354fec2ea1a0229f6f70073280699f3810b
Bartosz Pajewski <bartoszpajewski@gmail.com>
2019-06-02 20:15:11 +0200

    commit roboczy

--- filecollerctor.cpp
+++ filecollerctor.cpp
@@ -12,7 +12,11 @@ FileCollerctor::FileCollerctor(const QString &catalogName, QObject *parent)
 
 FileCollerctor::FileCollerctor(const QDir &path, QObject *parent)
 {
-
+    _path = path;
+    if(!_path.exists())
+    {
+        // rzuć błędem
+    }
 }
 
 void FileCollerctor::run()
